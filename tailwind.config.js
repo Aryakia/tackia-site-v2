4621f2022bede3680ce81c44676df951a1050653
Arya Kia <Aryakia@mit.edu>
2025-11-06 02:35:38 -0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,17 +8,11 @@ module.exports = {
     extend: {
       colors: {
         ink: "#0B0B0F",
-        primary: {
-          DEFAULT: "#FF007A",
-          dark: "#D60067"
-        },
-        secondary: {
-          DEFAULT: "#00D1FF",
-          dark: "#00A7CC"
-        },
+        primary: { DEFAULT: "#FF007A", dark: "#D60067" },
+        secondary: { DEFAULT: "#00D1FF", dark: "#00A7CC" },
         yellow: "#FFD600",
         orange: "#FF6B00",
-        green: "#32D296"
+        green: "#32D296",
       },
     },
   },
